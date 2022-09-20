5a9d60e7956787d6f3d8171ac58b4bd44b64617e
Gudkova Mariia <gudmary8@gmail.com>
2022-09-21 00:20:30 +0400

    mobile_header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,56 @@ import headerLogo from "../images/header__logo.svg";
 function Header(props) {
   const { loggedIn, buttonText } = props;
   const location = useLocation();
-  return (
+  const [open, setOpen] = React.useState(false);
+  const close = () => setOpen(false);
+  function onClick() {
+    return setOpen(!open);
+  }
+  return { loggedIn } && location.pathname === "/" ? (
+    <header
+      className={
+        !open ? "header header_auth" : "header header_auth header_auth-active"
+      }
+    >
+      <img className="header__logo" src={headerLogo} alt="Логотип" />
+      <div
+        className={!open ? "header__menu" : "header__menu header__menu_active"}
+      >
+        <p className="header__email-info header__email-info_auth">
+          myemail@email
+        </p>
+        <Link
+          to="/sign-in"
+          className=" header__link-button header__link-button_auth"
+        >
+          {`${buttonText}`}
+        </Link>
+      </div>
+      <ul
+        className={!open ? "header__burger_active" : "header__burger"}
+        onClick={onClick}
+      >
+        <li className="header__burger-line" />
+        <li className="header__burger-line" />
+        <li className="header__burger-line" />
+      </ul>
+      <ul
+        className={
+          !open ? "header__cross" : "header__cross header__cross_active"
+        }
+        onClick={close}
+      >
+        <li className="header__cross-line" />
+        <li className="header__cross-line" />
+      </ul>
+    </header>
+  ) : (
     <header className="header">
       <img className="header__logo" src={headerLogo} alt="Логотип" />
-      {{ loggedIn } && location.pathname === "/" ? (
-        <>
-          <p className="header__email-info">myemail@email</p>
-          <Link to="/sign-in" className="header__link-button">
-            {`${buttonText}`}
-          </Link>
-        </>
-      ) : (
-        <Link
-          to={location.pathname === "/sign-in" ? "/sign-up" : "/sign-in"}
-          className="header__link-button"
-        >{`${buttonText}`}</Link>
-      )}
+      <Link
+        to={location.pathname === "/sign-in" ? "/sign-up" : "/sign-in"}
+        className="header__link-button"
+      >{`${buttonText}`}</Link>
     </header>
   );
 }
